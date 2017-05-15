9645ae17a9b57a2b544ac9125dfa119b1f34abe3
kabelak <kavin.pl.shop@gmail.com>
2017-05-15 16:05:24 +0100

    Initial commit

--- parse_hemedistance.py
+++ parse_hemedistance.py
@@ -5,7 +5,7 @@ import sys
 import re
 import argparse
 import os
-
+from openpyxl import *
 
 def check_range(arg):  # Function to ensure correct range of IRE Length is input at the command prompt
     try:
@@ -16,6 +16,16 @@ def check_range(arg):  # Function to ensure correct range of IRE Length is input
         message = "Expected length between 1 and 30 inclusive, received value = {}".format(value)
         raise argparse.ArgumentTypeError(message)
     return value
+
+
+def namerows(_sheet):
+    #   Create row headings
+    _sheet.cell(row=3, column=1).value = "Omega 6"
+    _sheet.cell(row=4, column=1).value = "Omega 9"
+    _sheet.cell(row=5, column=1).value = "Omega 12"
+    _sheet.cell(row=6, column=1).value = "Omega 15"
+    _sheet.cell(row=7, column=1).value = "Carbon 19"
+    return _sheet
 
 
 def main(argv):
@@ -45,11 +55,83 @@ def main(argv):
                 if float(dist_cols.group(2)) < args.cutoff[0]:
                     relevant_frames += 1
 
-        print('\tFile Name:\t', args.file, '\n',
-              '\tTotal Frames:\t', total_frames, '\n',
-              '\t< cutoff:\t', relevant_frames, '\n',
-              '\t% < cutoff:\t', (relevant_frames / total_frames * 100))
+                    #      print('\tFile Name:\t', args.file, '\n',
+                    #           '\tTotal Frames:\t', total_frames, '\n',
+                    #          '\tCutoff Value:\t', args.cutoff[0], '\n',
+                    #         '\t< cutoff :\t', relevant_frames, '\n',
+                    #        '\t% < cutoff:\t', round(relevant_frames / total_frames * 100, 2))
+
+    # A bit of naming maintenace, to be used throughout
+    fname = re.search('(Docked_Sims).*/Distance_FEto(.*)\.(\d)\.(.*)\.agr', args.file)
+    fname2 = str(fname.group(1)) + '_hemedistance_analysed.xlsx'
+
+    print(fname.group(1), fname.group(2), fname.group(3), fname.group(4))
+    # main_name = fname.group(1)
+    bond = fname.group(2)
+    pose = fname.group(3)
+    repeat = fname.group(4)
+
+    # print(os.path.abspath(args.file))
+
+    #   Create a workbook and set row 1
+    if os.path.exists(fname2):
+        wb = load_workbook(fname2)
+        if not str(args.cutoff[0]) in wb.sheetnames:
+            ws = wb.create_sheet(title=(str(args.cutoff[0])))
+            ws = namerows(ws)
+            _col = 2
+        else:
+            ws = wb[str(args.cutoff[0])]
+            # _row = ws.max_rows
+            _col = ws.max_column
+
+    else:
+        wb = Workbook()
+        ws = wb.create_sheet(title=(str(args.cutoff[0])))
+        ws = namerows(ws)
+        _col = 2
+
+    if not ws.cell(row=1, column=_col).value:
+        ws.cell(row=1, column=_col).value = pose
+
+    if not ws.cell(row=2, column=_col).value:
+        ws.cell(row=2, column=_col).value = str(repeat)
+    elif ws.cell(row=2, column=_col).value != str(repeat) and bond == "Omega6":
+        _col += 1
+
+    if bond == "Omega6":
+        ws.cell(row=3, column=_col).value = round(relevant_frames / total_frames * 100, 2)
+    elif bond == "Omega9":
+        ws.cell(row=4, column=_col).value = round(relevant_frames / total_frames * 100, 2)
+    elif bond == "Omega12":
+        ws.cell(row=5, column=_col).value = round(relevant_frames / total_frames * 100, 2)
+    elif bond == "Omega15":
+        ws.cell(row=6, column=_col).value = round(relevant_frames / total_frames * 100, 2)
+    elif bond == "C19":
+        ws.cell(row=7, column=_col).value = round(relevant_frames / total_frames * 100, 2)
+
+    # Save and exit, clearing the 'Sheet' worksheet
+    wb.save(fname2)
+    wb.close()
+    #   Delete empty first "Sheet"
+    wb_del = load_workbook(fname2)
+    if 'Sheet' in wb_del.sheetnames:
+        ws_del = wb_del.get_sheet_by_name("Sheet")
+        wb_del.remove_sheet(ws_del)
+        wb_del.save(fname2)
+
+    wb_del.close()
 
 
 if __name__ == "__main__":
     main(sys.argv)
+
+'''
+    #   Create row headings
+    if ws.cell(row=_row, column=_col).value:
+        _row += 1
+        ws.cell(row=_row, column=_col).value = bond
+    else:
+        ws.cell(row=_row, column=_col).value = bond
+
+'''

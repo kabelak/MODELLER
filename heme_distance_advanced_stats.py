24dbf608acba6ca56da707a29f49c0c91c0fd002
kabelak <kavin.pl.shop@gmail.com>
2017-08-24 15:28:53 +0100

    Initial commit

--- heme_distance_advanced_stats.py
+++ heme_distance_advanced_stats.py
@@ -15,25 +15,58 @@ def skipvalue(_filename):
     return _skip
 
 
-for o in ['Omega6', 'Omega9', 'Omega12', 'Omega15', 'C19', 'CO2']:
-    for i in range(1, 7):
-        _file = "/Users/Kavin/phd/MD/Simulation4/Docked_Sims/State_%d/007.cpptraj/Distance_FEto%s.%d.old.agr" % (
-        i, o, i)
-        _skip = skipvalue(_file)
-        r.assign('dataf', os.path.abspath(_file))
-        r.assign('skippy', _skip)
-        r('dc <- read.table(dataf, skip=skippy, col.names = c("Time", "Distance"))')
-        r('d <- dc[,"Distance"]')
-        # print(r('median(d)')[0])
+_root = "/Users/Kavin/phd/MD/Simulation4"
+for _sim in ["/Docked_Sims", "/Mut_R111A", "/Mut_R117A", "/Mut_R111A_R117A", "/Mut_quadruple"]:
+    # The type here is either wildtype (1) or mutants (0)
+    print("%s; Median; Mean; SD" % (_sim))
+    type = 0
+    if _sim == "/Docked_Sims":
+        type = 1
 
-        for j in range(1, 3):
-            _file = "/Users/Kavin/phd/MD/Simulation4/Docked_Sims/State_%d/REPEATS/%d/007.cpptraj/Distance_FEto%s.%d.%d.agr" % (
-            i, j, o, i, j)
-            _skip = skipvalue(_file)
-            r.assign('dataf', os.path.abspath(_file))
-            r.assign('skippy', _skip)
-            r('dc <- read.table(dataf, skip=skippy, col.names = c("Time", "Distance"))')
-            r('d <- rbind(d, dc[,"Distance"])')
+    for o in ['Omega6', 'Omega9', 'Omega12', 'Omega15', 'C19', 'CO2']:
+        for i in range(1, 7):
+            if type == 1:
+                _directory = "/State_%d/007.cpptraj/Distance_FEto%s.%d.old.agr" % (i, o, i)
+                _file = _root + _sim + _directory
+                _skip = skipvalue(_file)
 
-        print("Pose: %d; Omega: %s" % (i, o))
-        print("Median post 100ns:", r('median(d)')[0])
+                print(os.path.abspath(_file))
+
+                r.assign('dataf', os.path.abspath(_file))
+                r.assign('skippy', _skip)
+                r('dc <- read.table(dataf, skip=skippy, col.names = c("Time", "Distance"))')
+                if i == 1:
+                    r('d <- dc[,"Distance"]')
+                else:
+                    r('d <- rbind(d, dc[,"Distance"])')
+
+            for j in range(1, 4):
+                _directory = "/State_%d/REPEATS/%d/007.cpptraj/Distance_FEto%s.%d.%d.agr" % (i, j, o, i, j)
+                _file = _root + _sim + _directory
+                _skip = skipvalue(_file)
+                r.assign('dataf', os.path.abspath(_file))
+                r.assign('skippy', _skip)
+                r('dc <- read.table(dataf, skip=skippy, col.names = c("Time", "Distance"))')
+
+                if type == 1:
+                    r('d <- rbind(d, dc[,"Distance"])')
+                else:
+                    if j == 1:
+                        r('d <- dc[,"Distance"]')
+                    else:
+                        r('d <- rbind(d, dc[,"Distance"])')
+
+                        # print("Pose: %d; %s" % (i, o))
+                        # print("Median post 100ns:", r('median(d)')[0])
+                        # print("Pose %d; %f; %s" % (i, r('median(d)')[0], o))
+                        # print("Median post 100ns:", r('median(d)')[0])
+        # print("Pose %d; %f; %s" % (i, r('median(d)')[0], o))
+
+        print("%s; %f; %f; %f" % (o, r('median(d)')[0], r('mean(d)')[0], r('sd(d)')[0]))
+        # r('hist(d)')
+        # input("\nPress Enter to continue.")
+
+        # Save the dataframe as .Rdata for future use
+        Rdataout = _root + _sim + "/" + o + "combined.Rdata"
+        r.assign('fileout', Rdataout)
+        r('save(d, file=fileout)')
--- testnew.py
+++ testnew.py
@@ -8,39 +8,11 @@ import os
 from openpyxl import *
 from rpy2.robjects import r
 
+_root = "/Users/Kavin/phd/MD/Simulation4"
+_sim = "/Docked_Sims"
+
 for o in ['Omega6', 'Omega9', 'Omega12', 'Omega15', 'C19', 'CO2']:
     for i in range(1, 7):
-        _fileold = "/Users/Kavin/phd/MD/Simulation4/Docked_Sims/State_%d/007.cpptraj/Distance_FEto%s.%d.old.agr" % (
-        i, o, i)
-        _file = _fileold
-
-        if "Mut" in _file:
-            _skip = 10008
-        else:
-            _skip = 5008
-
-        r.assign('dataf', os.path.abspath(_file))
-        r.assign('skippy', _skip)
-        r('dc <- read.table(dataf, skip=skippy, col.names = c("Time", "Distance"))')
-        r('d <- dc[,"Distance"]')
-
-        # print(r('median(d)')[0])
-
-        for j in range(1, 3):
-            _filerep = "/Users/Kavin/phd/MD/Simulation4/Docked_Sims/State_%d/REPEATS/%d/007.cpptraj/Distance_FEto%s.%d.%d.agr" % (
-            i, j, o, i, j)
-            # print(_fileold, _filerep)
-            _file = _filerep
-
-            if "Mut" in _file:
-                _skip = 10008
-            else:
-                _skip = 5008
-
-            r.assign('dataf', os.path.abspath(_file))
-            r.assign('skippy', _skip)
-            r('dc <- read.table(dataf, skip=skippy, col.names = c("Time", "Distance"))')
-            r('d <- rbind(d, dc[,"Distance"])')
-
-        print("Pose: %d; Omega: %s" % (i, o))
-        print(r('median(d)')[0])
+        _directory = "/State_%d/007.cpptraj/Distance_FEto%s.%d.old.agr" % (i, o, i)
+        _file = _root + _sim + _directory
+        print(_file)
